6f760693fa0f26867f2394dea6a93092bc74542d
Pedro Del Rio Ortiz <pedro.ortiz.3@globalhitss.com.br>
2023-06-22 01:36:26 -0300

    Initial commit

---
+++ TVMazeApp/TVMazeApp/TVMazeAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TVMazeAppApp.swift
+//  TVMazeApp
+//
+//  Created by HITSS on 22/06/23.
+//
+
+import SwiftUI
+
+@main
+struct TVMazeAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
